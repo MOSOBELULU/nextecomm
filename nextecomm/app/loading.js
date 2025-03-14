afac93bce1a2a570322dd6319488f1e05068cde3
Lulu Mosobe <125878495+MOSOBELULU@users.noreply.github.com>
2025-03-14 09:31:11 +0200

    wip

--- nextecomm/app/loading.js
+++ nextecomm/app/loading.js
@@ -1,7 +1,12 @@
 export default function Loading() {
     return (
-        <div className="d-flex justify-content-center align-items-center vh-100 text-danger">
-            Loading...
+        <div className="d-flex justify-content-center align-items-center vh-100">
+            <div className="text-center">
+                <div className="spinner-border text-danger" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </div>
+                {/* <p className="mt-2 text-danger">Loading...</p> */}
+            </div>
         </div>
-    )
+    );
 }
